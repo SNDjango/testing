777ce697aaf55e6ca4f0c080a2991f150948a27a
pr0me <g33sus@gmail.com>
2017-05-18 22:01:41 +0200

    added ex. authentication

--- snd/image_board/urls.py
+++ snd/image_board/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^login$', views.login_page, name='login_page'),
+    url(r'^profile$', views.profile, name='profile'),
+    url(r'^signup$', views.signup, name='signup'),
+    url(r'^logout$', views.logout_page, name='logout_page'),
 
 ]
--- snd/image_board/views.py
+++ snd/image_board/views.py
@@ -3,22 +3,51 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView,
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.contrib.auth.views import redirect_to_login
+from django.conf import settings
 
 def index(request):
     return render(request, 'index.html')
 
-def login(request):
+
+def profile(request):
+    if not request.user.is_authenticated:
+        return redirect_to_login('profile', login_url='login_page')
+    else:
+        return render(request, 'profile.html')
+
+
+def login_page(request):
     if(request.method == 'POST'):
-        user = request.POST['user']
+        name = request.POST['user']
         pwd = request.POST['pwd']
 
-        user = authenticate(username=user, password=pwd)
+        user = authenticate(username=name, password=pwd)
         if user is not None:
             login(request, user)
-            return redirect(index)
-
+            return redirect('index')
+        else:
+            return render(request, 'error_login.html')
 
     else:
         return render(request, 'login.html')
+
+
+def signup(request):
+    if(request.method == 'POST'):
+        name = request.POST['user']
+        email = request.POST['email']
+        pwd = request.POST['pwd']
+
+        user = User.objects.create_user(name, email, pwd)
+        return redirect('index')
+
+    else:
+        return render(request, 'signup.html')
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('index')
